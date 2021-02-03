ee37f5004201b20e3ab21ad3284dad57f5204512
Justin Meyer <meyerj.justin@gmail.com>
2021-02-02 21:52:15 -0500

    complete reverseBits

--- leetcode/numberOneBits.js
+++ leetcode/numberOneBits.js
@@ -1,6 +1,6 @@
 var hammingWeight = function (n) {
     count = 0
-    for (let digit = 0; digit < 31; digit++) {
+    for (let digit = 0; digit < 32; digit++) {
         console.log(`n: ${n}`)
         if (n % 2 === 1) count++;
         n = Math.floor(n / 2);
